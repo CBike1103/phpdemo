e9d1cdc33bdec103eab801fdd48dad5b1b9b8085
Wuvist <wuvist@gmail.com>
2018-05-31 13:44:25 +0800

    Add code

--- test.php
+++ test.php
@@ -1,9 +1,13 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+require __DIR__ . '/vendor/autoload.php';
+
 use LinkORB\Component\Etcd\Client;
 
 $client = new Client();
 $client->set('/foo', 'fooValue');
 // Set the ttl
-$client->set('/foo', 'fooValue', 10);
+// $client->set('/foo', 'fooValue', 10);
 // get key value
 echo $client->get('/foo');
